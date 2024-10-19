81c6539674ed79bc4e3ce856658dac18a438ba07
Muhammad Haroon <mharoon1326@gmail.com>
2024-10-19 16:09:26 +0500

    init

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,60 +1,86 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
-
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
+void main() {
+  runApp(MyApp());
 }
 
-class _MyAppState extends State<MyApp> {
-  double _width = 50;
-  double _height = 50;
-  Color _color = Colors.green;
-  BorderRadiusGeometry _borderRadius = BorderRadius.circular(8);
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Animated Container Demo'),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center, // Center the column contents
-          children: [
-            Center(
-              child: AnimatedContainer(
-                width: _width,
-                height: _height,
-                decoration: BoxDecoration(
-                  color: _color,
-                  borderRadius: _borderRadius,
-                ),
-                duration: const Duration(seconds: 2),
-                curve: Curves.fastOutSlowIn,
+      home: AnimatedContainerDemo(),
+    );
+  }
+}
+
+class AnimatedContainerDemo extends StatefulWidget {
+  @override
+  _AnimatedContainerDemoState createState() => _AnimatedContainerDemoState();
+}
+
+class _AnimatedContainerDemoState extends State<AnimatedContainerDemo> {
+  double _height = 100.0;
+  double _width = 100.0;
+  Color _color = Colors.blue;
+  BorderRadiusGeometry _borderRadius = BorderRadius.circular(8);
+  bool _isVisible = true;
+
+  void _animateContainer() {
+    setState(() {
+      _height = _height == 100.0 ? 50.0 : 200.0;
+      _width = _width == 100.0 ? 50.0 : 200.0;
+      _color = _color == Colors.blue ? Colors.red : Colors.blue;
+      _borderRadius = _borderRadius == BorderRadius.circular(8) 
+          ? BorderRadius.circular(30) 
+          : BorderRadius.circular(8);
+    });
+  }
+
+  void _toggleTextVisibility() {
+    setState(() {
+      _isVisible = !_isVisible;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Simple-Animations-using-Implicit-Widgets'
+          ),
+          centerTitle: true),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            AnimatedContainer(
+              duration: Duration(seconds: 1),
+              height: _height,
+              width: _width,
+              decoration: BoxDecoration(
+                color: _color,
+                borderRadius: _borderRadius,
               ),
             ),
-            SizedBox(height: 20), // Add some space between the container and the button
-            FloatingActionButton(
+            SizedBox(height: 50),
+            AnimatedOpacity(
+              opacity: _isVisible ? 1.0 : 0.0,
+              duration: Duration(seconds: 1),
+              child: Text(
+                'Implicit Widget',
+                style: TextStyle(
+                  fontSize: 16
+                  ),
+              ),
+            ),
+            SizedBox(height: 50),
+            ElevatedButton(
               onPressed: () {
-                setState(() {
-                  final random = Random();
-                  _width = random.nextInt(300).toDouble();
-                  _height = random.nextInt(300).toDouble();
-                  _color = Color.fromRGBO(
-                    random.nextInt(256),
-                    random.nextInt(256),
-                    random.nextInt(256),
-                    1,
-                  );
-                  _borderRadius = BorderRadius.circular(random.nextInt(100).toDouble());
-                });
+                _animateContainer();
+                _toggleTextVisibility();
               },
-              child: Icon(Icons.play_arrow),
+              child: Text('Click To Toggle Text Visibility!'),
             ),
           ],
         ),
